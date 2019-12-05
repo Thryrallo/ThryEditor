73c14c0d60d2adbe69d73fd6cf6b62bdcb916113
Thryrallo <thryrallo@gmail.com>
2019-12-05 14:07:24 +0100

    v1.2a2
    - added alpha and beta for version parsing

--- Editor/ThryConfig.cs
+++ Editor/ThryConfig.cs
@@ -13,7 +13,7 @@ namespace Thry
     {
         // consts
         private const string PATH_CONFIG_FILE = "Thry/Config.json";
-        private const string VERSION = "1.0.3";
+        private const string VERSION = "1.2a2";
 
         // static
         private static Config config;
--- Editor/ThryHelper.cs
+++ Editor/ThryHelper.cs
@@ -66,26 +66,63 @@ namespace Thry
 
         //-------------------Comparetors----------------------
 
-        /// <summary>
-        /// -1 if v1 > v2
-        /// 0 if v1 == v2
-        /// 1 if v1 < v2
-        /// </summary>
         public static int compareVersions(string v1, string v2)
         {
-            Match v1_match = Regex.Match(v1, @"\d+(\.\d+)*");
-            Match v2_match = Regex.Match(v2, @"\d+(\.\d+)*");
+            //fix the string
+            Match v1_match = Regex.Match(v1, @"(a|b)?\d+((\.|a|b)\d+)*(a|b)?");
+            Match v2_match = Regex.Match(v2, @"(a|b)?\d+((\.|a|b)\d+)*(a|b)?");
             if (!v1_match.Success && !v2_match.Success) return 0;
             else if (!v1_match.Success) return 1;
             else if (!v2_match.Success) return -1;
-            string[] v1Parts = Regex.Split(v1_match.Value, @"\.");
-            string[] v2Parts = Regex.Split(v2_match.Value, @"\.");
-            for (int i = 0; i < Math.Max(v1Parts.Length, v2Parts.Length); i++)
+            v1 = v1_match.Value;
+            v2 = v2_match.Value;
+
+            int index_v1 = 0;
+            int index_v2 = 0;
+            string chunk_v1;
+            string chunk_v2;
+            while (index_v1 < v1.Length || index_v2 < v2.Length)
             {
-                if (i >= v1Parts.Length) return 1;
-                else if (i >= v2Parts.Length) return -1;
-                int v1P = int.Parse(v1Parts[i]);
-                int v2P = int.Parse(v2Parts[i]);
+                //get a chunk of the strings
+                if (index_v1 < v1.Length){
+                    chunk_v1 = "";
+                    if (v1[index_v1] == 'a')
+                        chunk_v1 = "-2";
+                    else if (v1[index_v1] == 'b')
+                        chunk_v1 = "-1";
+                    else
+                    {
+                        while (index_v1 < v1.Length && v1[index_v1] != 'a' && v1[index_v1] != 'b' && v1[index_v1] != '.')
+                            chunk_v1 += v1[index_v1++];
+                        if (index_v1 < v1.Length && (v1[index_v1] == 'a' || v1[index_v1] == 'b'))
+                            index_v1--;
+                    }
+                    index_v1++;
+                }else
+                    chunk_v1 = "0";
+
+                if (index_v2 < v2.Length)
+                {
+                    chunk_v2 = "";
+                    if (v2[index_v2] == 'a')
+                        chunk_v2 = "-2";
+                    else if (v2[index_v2] == 'b')
+                        chunk_v2 = "-1";
+                    else
+                    {
+                        while (index_v2 < v2.Length && v2[index_v2] != 'a' && v2[index_v2] != 'b' && v2[index_v2] != '.')
+                            chunk_v2 += v2[index_v2++];
+                        if (index_v2 < v2.Length && (v2[index_v2] == 'a' || v2[index_v2] == 'b'))
+                            index_v2--;
+                    }
+                    index_v2++;
+                }
+                else
+                    chunk_v2 = "0";
+
+                //compare chunks
+                int v1P = int.Parse(chunk_v1);
+                int v2P = int.Parse(chunk_v2);
                 if (v1P > v2P) return -1;
                 else if (v1P < v2P) return 1;
             }
