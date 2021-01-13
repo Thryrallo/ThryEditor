6c8331d9060171ba4f5588ccb443d848e5eae2a9
Thryrallo <thryrallo@gmail.com>
2021-01-13 20:16:24 +0100

    2.0.1

--- Editor/Drawers.cs
+++ Editor/Drawers.cs
@@ -380,6 +380,52 @@ namespace Thry
         }
     }
 
+    public enum ColorMask
+    {
+        None,
+        Alpha,
+        Blue,
+        BA,
+        Green,
+        GA,
+        GB,
+        GBA,
+        Red,
+        RA,
+        RB,
+        RBA,
+        RG,
+        RGA,
+        RGB,
+        RGBA
+    }
+
+    // DX11 only blend operations
+    public enum BlendOp
+    {
+        Add,
+        Subtract,
+        ReverseSubtract,
+        Min,
+        Max,
+        LogicalClear,
+        LogicalSet,
+        LogicalCopy,
+        LogicalCopyInverted,
+        LogicalNoop,
+        LogicalInvert,
+        LogicalAnd,
+        LogicalNand,
+        LogicalOr,
+        LogicalNor,
+        LogicalXor,
+        LogicalEquivalence,
+        LogicalAndReverse,
+        LogicalAndInverted,
+        LogicalOrReverse,
+        LogicalOrInverted
+    }
+
     //Original Code from https://github.com/DarthShader/Kaj-Unity-Shaders
     /**MIT License
 
