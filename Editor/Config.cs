654dffc0bf85965667a3752153b0f8505f5033ec
Thryrallo <thryrallo@gmail.com>
2021-02-25 14:31:32 +0100

    2.4.b1
    - testing the hacky fix for dissapearing texture

--- Editor/Config.cs
+++ Editor/Config.cs
@@ -13,7 +13,7 @@ namespace Thry
     {
         // consts
         private const string PATH_CONFIG_FILE = "Thry/Config.json";
-        private const string VERSION = "2.4";
+        private const string VERSION = "2.4.b1";
 
         // static
         private static Config config;
