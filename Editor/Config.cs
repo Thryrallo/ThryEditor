0f0da2b95b275467c49db266aa627a00bfd4815e
Thryrallo <thryrallo@gmail.com>
2021-03-05 15:30:28 +0100

    v2.8.0 config

--- Editor/Config.cs
+++ Editor/Config.cs
@@ -13,7 +13,7 @@ namespace Thry
     {
         // consts
         private const string PATH_CONFIG_FILE = "Thry/Config.json";
-        private const string VERSION = "2.7.4";
+        private const string VERSION = "2.8.0";
 
         // static
         private static Config config;
