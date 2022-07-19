77a912a815f03ec0a47b856d1269930d73f1c18f
Thryrallo <thryrallo@gmail.com>
2022-07-19 15:25:16 +0200

    v2.27.2
    - fixed locking for Hidden shaders.
    - hopefully didnt break unlocking of materials locked with legacy optimizer

--- Editor/Config.cs
+++ Editor/Config.cs
@@ -16,7 +16,7 @@ namespace Thry
     {
         // consts
         private const string PATH_CONFIG_FILE = "Thry/Config.json";
-        private const string VERSION = "2.27.1";
+        private const string VERSION = "2.27.2";
 
         // static
         private static Config config;
--- Editor/Drawers.cs
+++ Editor/Drawers.cs
@@ -1302,8 +1302,8 @@ namespace Thry
         {
             Material material = shaderOptimizer.targets[0] as Material;
             Shader shader = material.shader;
-            bool isLocked = shader.name.StartsWith("Hidden/") && 
-                (material.GetTag("OriginalShader",false,"") != "" || shader.GetPropertyDefaultFloatValue(shader.FindPropertyIndex(shaderOptimizer.name)) == 1);
+            bool isLocked = shader.name.StartsWith("Hidden/Locked/") || (shader.name.StartsWith("Hidden/") && 
+                (material.GetTag("OriginalShader",false,"") != "" && shader.GetPropertyDefaultFloatValue(shader.FindPropertyIndex(shaderOptimizer.name)) == 1));
             //this will make sure the button is unlocked if you manually swap to an unlocked shader
             //shaders that have the ability to be locked shouldnt really be hidden themself. at least it wouldnt make too much sense
             if (shaderOptimizer.hasMixedValue == false && shaderOptimizer.floatValue == 1 && isLocked == false)
