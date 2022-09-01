8245697f1c2c2a37d5a5840b3dffa1ac7491a62a
Thryrallo <thryrallo@gmail.com>
2022-09-01 11:44:16 +0200

    v2.27.8
    - added [ThrySeperator(hexColor)] Decorator

--- Editor/Drawers.cs
+++ Editor/Drawers.cs
@@ -738,6 +738,29 @@ namespace Thry
 #endregion
 
     #region Decorators
+    public class ThrySeperatorDecorator : MaterialPropertyDrawer
+    {
+        Color _color = Styles.COLOR_FG;
+
+        public ThrySeperatorDecorator() { }
+        public ThrySeperatorDecorator(string c)
+        {
+            ColorUtility.TryParseHtmlString(c, out _color);
+        }
+
+        public override float GetPropertyHeight(MaterialProperty prop, string label, MaterialEditor editor)
+        {
+            DrawingData.RegisterDecorator(this);
+            return 1;
+        }
+
+        public override void OnGUI(Rect position, MaterialProperty prop, string label, MaterialEditor editor)
+        {
+            position = EditorGUI.IndentedRect(position);
+            EditorGUI.DrawRect(position, _color);
+        }
+    }
+
     public class ThryHeaderLabelDecorator : MaterialPropertyDrawer
     {
         readonly string text;
