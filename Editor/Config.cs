7386e2c49500c0cc4ecd4a411c7d761df5ec28c7
Thryrallo <thryrallo@gmail.com>
2022-12-25 11:18:33 +0100

    v2.29.0
    The Locale Update version change

--- Editor/Config.cs
+++ Editor/Config.cs
@@ -16,7 +16,7 @@ namespace Thry
     {
         // consts
         private const string PATH_CONFIG_FILE = "Thry/Config.json";
-        private const string VERSION = "2.28.13";
+        private const string VERSION = "2.29.0";
 
         // static
         private static Config config;
