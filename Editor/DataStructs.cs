8395e53f0d8881e64565468be13e50a301705044
YP <yannick.pfeiffer@stud-mail.uni-wuerzburg.de>
2023-01-27 13:56:02 +0100

    v2.31.2
    - editor style textures now use guids for faster initilization

--- Editor/DataStructs.cs
+++ Editor/DataStructs.cs
@@ -47,11 +47,11 @@ namespace Thry
         public const string IMAGING_EXISTS = "IMAGING_DLL_EXISTS";
     }
 
-    public class RESOURCE_NAME
+    public class RESOURCE_GUID
     {
-        public const string RECT = "thry_rect";
-        public const string ICON_NAME_LINK = "thryEditor_link";
-        public const string ICON_NAME_THRY = "thryEditor_iconThry";
+        public const string RECT = "2329f8696fd09a743a5baf2a5f4986af";
+        public const string ICON_LINK = "e85fd0a0e4e4fea46bb3fdeab5c3fb07";
+        public const string ICON_THRY = "693aa4c2cdc578346a196469a06ddbba";
     }
     #endregion
 
