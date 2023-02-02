8607806c6778213c99bbc0a9e89f4e6d18b5d099
YP <yannick.pfeiffer@stud-mail.uni-wuerzburg.de>
2023-02-02 12:15:14 +0100

    v2.31.13
    - channel preview in texture packer was not technically correct (not sure if this is better for low level users understanding of the packing or worse, since technically it shows the process better) But definitly better for actually packing stuff

--- Editor/Config.cs
+++ Editor/Config.cs
@@ -16,7 +16,7 @@ namespace Thry
     {
         // consts
         private const string PATH_CONFIG_FILE = "Thry/Config.json";
-        private const string VERSION = "2.31.12";
+        private const string VERSION = "2.31.13";
 
         // static
         private static Config config;
