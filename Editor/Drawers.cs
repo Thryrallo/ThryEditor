d0ddbc9617d28c07be47f61a4ba45687e3bc4864
Thryrallo <thryrallo@gmail.com>
2023-12-09 15:14:54 +0100

    v2.47.3
    - i dont even know, i wanna cry, why does unity do this shit. this is so random. i dont get it, what was wrong? Did EditorStyle not exist during constructor call times? If so why is that not a problem in unity 2019? Is the warning ignored or did the unity team fuck up. Please, this is purgetory, why is unity so cursed ;(

--- Editor/Drawers.cs
+++ Editor/Drawers.cs
@@ -947,61 +947,71 @@ namespace Thry
 
     public class ThryHeaderLabelDecorator : MaterialPropertyDrawer
     {
-        readonly string text;
-        readonly int size;
-        GUIStyle style;
+        readonly string _text;
+        readonly int _size;
+        GUIStyle _style;
 
         public ThryHeaderLabelDecorator(string text) : this(text, EditorStyles.standardFont.fontSize)
         {
         }
         public ThryHeaderLabelDecorator(string text, float size)
         {
-            this.text = text;
-            this.size = (int)size;
-            style = new GUIStyle(EditorStyles.boldLabel);
-            style.fontSize = this.size;
+            this._text = text;
+            this._size = (int)size;
         }
 
 
         public override float GetPropertyHeight(MaterialProperty prop, string label, MaterialEditor editor)
         {
             DrawingData.RegisterDecorator(this);
-            return size + 6;
+            return _size + 6;
         }
 
         public override void OnGUI(Rect position, MaterialProperty prop, string label, MaterialEditor editor)
         {
+            // Done here instead of constructor because else unity throws warnings
+            if(_style == null)
+            {
+                _style = new GUIStyle(EditorStyles.boldLabel);
+                _style.fontSize = this._size;
+            }
+
             float offst = position.height;
             position = EditorGUI.IndentedRect(position);
-            GUI.Label(position, text, style);
+            GUI.Label(position, _text, _style);
         }
     }
 
     public class ThryRichLabelDrawer : MaterialPropertyDrawer
     {
-        readonly int size;
-        GUIStyle style;
+        readonly int _size;
+        GUIStyle _style;
 
         public ThryRichLabelDrawer(float size)
         {
-            this.size = (int)size;
-            style = new GUIStyle(EditorStyles.boldLabel);
-            style.richText = true;
-            style.fontSize = this.size;
+            this._size = (int)size;
         }
 
         public ThryRichLabelDrawer() : this(EditorStyles.standardFont.fontSize) { }
 
         public override float GetPropertyHeight(MaterialProperty prop, string label, MaterialEditor editor)
         {
-            return size + 4;
+            return _size + 4;
         }
 
         public override void OnGUI(Rect position, MaterialProperty prop, string label, MaterialEditor editor)
         {
+            // Done here instead of constructor because else unity throws warnings
+            if(_style == null)
+            {
+                _style = new GUIStyle(EditorStyles.boldLabel);
+                _style.richText = true;
+                _style.fontSize = this._size;
+            }
+
             float offst = position.height;
             position = EditorGUI.IndentedRect(position);
-            GUI.Label(position, label, style);
+            GUI.Label(position, label, _style);
         }
     }
     #endregion
