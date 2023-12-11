64dc6215b4a0bce48bc25cae28f8ec3716b1c11b
Thryrallo <thryrallo@gmail.com>
2023-12-11 20:09:40 +0100

    Update EditorStructs.cs

--- Editor/EditorStructs.cs
+++ Editor/EditorStructs.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
-using PlasticGui.WorkspaceWindow.Home;
 using Thry.ThryEditor;
 using UnityEditor;
 using UnityEngine;
