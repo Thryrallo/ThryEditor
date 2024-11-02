7dad227bf52b94c6878f867e36d3daf133322397
Thryrallo <thryrallo@gmail.com>
2024-11-02 14:26:25 +0100

    v2.58.6
    - Added Way to retieve drawer type from ShaderProperty
    - added rich text to ListTogglesPopup

--- Editor/Config.cs
+++ Editor/Config.cs
@@ -10,7 +10,7 @@ namespace Thry
     public class Config
     {
         private const string PATH_CONFIG_FILE = "Thry/Config.json";
-        private const string VERSION = "2.58.5";
+        private const string VERSION = "2.58.6";
 
         private static Config config;
 
