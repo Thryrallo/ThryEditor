d951363e2fb5bd0207ad492392dcd332f0b62924
Thryrallo <thryrallo@gmail.com>
2025-08-22 12:58:39 +0200

    v2.67.0
    [New] ThrySpace Decorator
    [Change] Added Options to ThrySeperator
    [Fix] Fixed namespace in Vector31.cs

--- Editor/Decorators/ThrySeperator.cs
+++ Editor/Decorators/ThrySeperator.cs
@@ -5,23 +5,64 @@ namespace Thry.ThryEditor.Decorators
 {
     public class ThrySeperatorDecorator : MaterialPropertyDrawer
     {
-        Color _color = Colors.foreground;
+        readonly Color _color;
+        readonly float _thickness;
+        readonly float _paddingTop;
+        readonly float _paddingBottom;
 
-        public ThrySeperatorDecorator() { }
-        public ThrySeperatorDecorator(string c)
+        public ThrySeperatorDecorator() : this(1, 0, 0)
         {
+        }
+        
+        public ThrySeperatorDecorator(float thickness) : this(thickness, 5, 5)
+        {
+        }
+        
+        public ThrySeperatorDecorator(float thickness, float padding) : this(thickness, padding, padding)
+        {
+        }
+        
+        public ThrySeperatorDecorator(float thickness, float paddingTop, float paddingBottom)
+        {
+            this._color = Colors.backgroundDark;
+            this._thickness = thickness;
+            this._paddingTop = paddingTop;
+            this._paddingBottom = paddingBottom;
+        }
+        
+        public ThrySeperatorDecorator(string c) : this(c, 1, 0, 0)
+        {
+        }
+        
+        public ThrySeperatorDecorator(string c, float thickness) : this(c, thickness, 5, 5)
+        {
+        }
+        
+        public ThrySeperatorDecorator(string c, float thickness, float padding) : this(c, thickness, padding, padding)
+        {
+        }
+        
+        public ThrySeperatorDecorator(string c, float thickness, float paddingTop, float paddingBottom)
+        {
+            if (!c.StartsWith("#"))
+                c = "#" + c;
             ColorUtility.TryParseHtmlString(c, out _color);
+            this._thickness = thickness;
+            this._paddingTop = paddingTop;
+            this._paddingBottom = paddingBottom;
         }
 
         public override float GetPropertyHeight(MaterialProperty prop, string label, MaterialEditor editor)
         {
             ShaderProperty.RegisterDecorator(this);
-            return 1;
+            return _thickness + _paddingTop + _paddingBottom;
         }
 
         public override void OnGUI(Rect position, MaterialProperty prop, string label, MaterialEditor editor)
         {
             position = EditorGUI.IndentedRect(position);
+            position.y += _paddingTop;
+            position.height = _thickness;
             EditorGUI.DrawRect(position, _color);
         }
     }
